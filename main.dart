d39d855bc78fecb0b7e3a3f6e42c2a3f5afe6cb7
deadhack <developerrohan01@gmail.com>
2026-04-10 10:50:18 -0400

    step 2 completed

--- main.dart
+++ main.dart
@@ -9,10 +9,7 @@ void main(){
   // print(t1.dueDate);
   // print(t2.dueDate);
 
-  final manager = TaskManager();
-  
-
-;
+  final manager = TaskManager();//final beacuse we create the manager once and never reassign it, but we can still add tasks to its list
 
 manager.addTask('Learn Dart Null Saftey');
 manager.addTask('Build CLI app', dueDate: '2026-04-01', tags: ['flutter', 'dart']);
@@ -26,7 +23,7 @@ for (final task in manager.tasks){
 manager.markDone(1);
 print('\nAfter marking task 1 as done:\n');
 for(final task in manager.tasks){
-  print('${task.id}. [${task.status}] $task.title');
+  print('${task.id}.[${task.status}] ${task.title}');
 }
 
 
